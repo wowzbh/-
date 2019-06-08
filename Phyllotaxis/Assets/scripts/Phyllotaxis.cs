ab6739b4121f59a3b3b4cb0a1418783b2a258370
朱博辉 <woozbh@163.com>
2019-06-08 22:17:06 +0800

    音效控制拖尾特效

--- Phyllotaxis/Assets/scripts/Phyllotaxis.cs
+++ Phyllotaxis/Assets/scripts/Phyllotaxis.cs
@@ -14,12 +14,15 @@ using System.Collections;
 
 /*TrailRenderer 组建实现拖尾
  * Lerping 控制拖尾运功轨迹与速度
- * 
+ *
+ *         Vector3.Lerp (from: Vector3, to: Vector3, t: float)
+ * t 在 [0 - 1], t = 0时 返回from, t =1 时返回 to, t = 0.5 时返回 from 与 to 的平均值
  */
- /*         Vector3.Lerp (from: Vector3, to: Vector3, t: float)
-  * t 在 [0 - 1], t = 0时 返回from, t =1 时返回 to, t = 0.5 时返回 from 与 to 的平均值
-  */
 public class Phyllotaxis : MonoBehaviour {
+
+    public AudioPeer _audioPeer;       //AudioPeer获得音乐文件的频谱
+    private Material _trailMat;             //拖尾材质
+    public Color _trailColor;                //拖尾颜色
 
     public GameObject _dot;
     //定义公式参数
@@ -31,13 +34,15 @@ public class Phyllotaxis : MonoBehaviour {
 
     //lerping 控制循环
     public bool _userlerping;  //用户控制是否使用Lerping
-    public float _intervalLerp;  //控制速度
     public int _stepSize;   //number 增加步数
     public int _maxIteration;  //执行lerp的最大次数
     private bool _isLerping;    //判断lerp的开始与结束
     private Vector3 _startPosition, _endPosition;
-    private float _timeStartedLerping;
     private int _currentIteration;   //当前Lerp的次数
+    private float _lerpPosTimer, _lerpPosSpeed;
+    public Vector2 _lerpPosSpeedMinMax;
+    public AnimationCurve _lerpPosAnimCurve;
+    public int _lerpPosBand;
 
     private Vector2 _phyllotaxisPosition;
     //Phyllotaxis计算坐标
@@ -52,10 +57,8 @@ public class Phyllotaxis : MonoBehaviour {
     }
 
     //lerping 初始化
-    void StartLerping()
+    void SetLerpPosition()
     {
-        _isLerping = true;
-        _timeStartedLerping = Time.time;
         _phyllotaxisPosition = CalculatePhyllotaxis(_degree, _scale, _number);
         _startPosition = this.transform.localPosition;
         _endPosition = new Vector3(_phyllotaxisPosition.x, _phyllotaxisPosition.y, 0);
@@ -66,14 +69,50 @@ public class Phyllotaxis : MonoBehaviour {
         _trailRenderer = this.GetComponent<TrailRenderer>();
         _number = _numberStart;
         this.transform.localPosition = CalculatePhyllotaxis(_degree, _scale, _number);
-
         if (_userlerping)
         {
-            StartLerping();
+            _isLerping = true;
+            SetLerpPosition();
         }
+
+        //设置_trailRenderer 的材质
+        _trailMat = new Material(_trailRenderer.material);
+        _trailMat.SetColor("_TintColor", _trailColor);  //TintColor
+        _trailRenderer.material = _trailMat;
+
     }
 
-    private void FixedUpdate()
+    private void Update()
+    {
+        if (_userlerping)
+        {
+            if (_isLerping)
+            {
+                _lerpPosSpeed = Mathf.Lerp(_lerpPosSpeedMinMax.x, _lerpPosSpeedMinMax.y, _lerpPosAnimCurve.Evaluate(_audioPeer._audioBand[_lerpPosBand]));
+                _lerpPosTimer = Time.deltaTime * _lerpPosSpeed;
+                //Clamp01 将变换的位置设为时间的位置，钳制在0 - 1
+                this.transform.localPosition = Vector3.Lerp(_startPosition, _endPosition, Mathf.Clamp01(_lerpPosTimer));
+                if (_lerpPosTimer >= 1)
+                {
+                    _lerpPosTimer -= 1;
+                    _number += _stepSize;
+                    SetLerpPosition();
+                }
+
+            }
+        }
+        if(!_userlerping)
+        {
+            //坐标随_number的增加而变化
+            _phyllotaxisPosition = CalculatePhyllotaxis(_degree, _scale, _number);
+            this.transform.localPosition = new Vector3(_phyllotaxisPosition.x, _phyllotaxisPosition.y, _lerpPosAnimCurve.Evaluate(_audioPeer._audioBand[_lerpPosBand]));
+
+
+            _number += _stepSize;
+            _currentIteration++;
+        }
+    }
+    /*private void FixedUpdate()
     {
         if (_userlerping)
         {
@@ -109,6 +148,6 @@ public class Phyllotaxis : MonoBehaviour {
             _number += _stepSize;
             _currentIteration++;
         }
-    }
+    }*/
 
 }
