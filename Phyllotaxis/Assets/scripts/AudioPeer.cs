90accc2eb25c68f3db15ccb781da786b76d374cc
朱博辉 <woozbh@163.com>
2019-06-08 22:17:06 +0800

    音效控制拖尾特效

---
+++ Phyllotaxis/Assets/scripts/AudioPeer.cs
@@ -0,0 +1,142 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(AudioSource))]
+public class AudioPeer : MonoBehaviour
+{
+
+    private AudioSource _audioSource;
+    private float[] _freqBand = new float[8];
+    private float[] _bandBuffer = new float[8];
+    private float[] _bufferDecrease = new float[8];
+
+    public float[] _freqBandHighest = new float[8];
+    [HideInInspector]
+    public float[] _audioBand, _audioBandBuffer;
+    //存储频谱信息的数组 左右声道
+    private float[] _samplesLeft = new float[512];
+    private float[] _samplesRight = new float[512];
+    //判断左右声道
+    public enum _channel { Stereo, Left, Right};
+    public _channel channel = new _channel();
+
+    //平滑变化
+    public float _audioProfile;
+    [HideInInspector]
+    public static float _Amplitude, _AmplitudeBuffer;
+    private float _AmplitudeHighest;
+    void Start()
+    {
+        //实例化音乐文件
+        _audioBand = new float[8];
+        _audioBandBuffer = new float[8];
+        _audioSource = this.GetComponent<AudioSource>();
+        AudioProfile(_audioProfile);
+    }
+
+    void Update()
+    {
+        GetSpectrumAudioSource();
+        MakeFrequencyBands();
+        BandBuffer();
+        CreateAudioBands();
+        GetAmplitude();
+    }
+
+    void GetAmplitude()
+    {
+        float _CurrentAmplitude = 0;
+        float _CurrentAmplitudeBuffer = 0;
+        for (int i = 0; i < 8; i++)
+        {
+            _CurrentAmplitude += _audioBand[i];
+            _CurrentAmplitudeBuffer += _audioBandBuffer[i];
+        }
+        if (_CurrentAmplitude > _AmplitudeHighest)
+        {
+            _AmplitudeHighest = _CurrentAmplitude;
+        }
+        _Amplitude = _CurrentAmplitude / _AmplitudeHighest;
+        _AmplitudeBuffer = _CurrentAmplitudeBuffer / _AmplitudeHighest;
+
+    }
+
+    void AudioProfile(float audioProfile)
+    {
+        for (int i = 0; i < 8; i++)
+        {
+            _freqBandHighest[i] = audioProfile;
+        }
+    }
+
+    void CreateAudioBands()
+    {
+        for (int i = 0; i < 8; i++)
+        {
+            if (_freqBand[i] > _freqBandHighest[i])
+            {
+                _freqBandHighest[i] = _freqBand[i];
+            }
+            _audioBand[i] = (_freqBand[i] / _freqBandHighest[i]);
+            _audioBandBuffer[i] = (_bandBuffer[i] / _freqBandHighest[i]);
+        }
+    }
+
+    void GetSpectrumAudioSource()
+    {
+        //获取音乐的频谱信息，并将频谱储存在_samples数组中
+        _audioSource.GetSpectrumData(_samplesLeft, 0, FFTWindow.Blackman);
+        _audioSource.GetSpectrumData(_samplesRight, 1, FFTWindow.Blackman);
+
+    }
+
+    void BandBuffer()
+    {
+        for (int i = 0; i < 8; ++i)
+        {
+            if (_freqBand[i] > _bandBuffer[i])
+            {
+                _bandBuffer[i] = _freqBand[i];
+                _bufferDecrease[i] = 0.005f;
+            }
+            if (_freqBand[i] < _bandBuffer[i])
+            {
+                _bandBuffer[i] -= _bufferDecrease[i];
+                _bufferDecrease[i] *= 1.2f;
+            }
+        }
+    }
+
+    void MakeFrequencyBands()
+    {
+        int count = 0;
+        for (int i = 0; i < 8; i++)
+        {
+            float average = 0;
+            int sampleCount = (int)Mathf.Pow(2, i) * 2;
+            if (i == 7)
+            {
+                sampleCount += 2;
+            }
+            for (int j = 0; j < sampleCount; j++)
+            {
+                if (channel == _channel.Stereo)
+                {
+                    average += (_samplesLeft[count] + _samplesRight[count]) * (count + 1);
+                }
+                if (channel == _channel.Left)
+                {
+                    average += _samplesLeft[count] * (count + 1);
+                }
+                if (channel == _channel.Right)
+                {
+                    average += _samplesRight[count] * (count + 1);
+
+                }
+                count++;
+            }
+            average /= count;
+            _freqBand[i] = average * 10;
+        }
+    }
+}
--- Phyllotaxis/Assets/scripts/PhyllotaxisTrail.cs
+++ Phyllotaxis/Assets/scripts/PhyllotaxisTrail.cs
@@ -1,25 +1,7 @@
 ﻿using UnityEngine;
-using System.Collections.Generic;
 using System.Collections;
 
-/* Phyllotaxis 公式  --极坐标公式
- *  angle:   φ = n * degree
- *  radius:   r = c * √n
- *  x = r * cos(angle)
- *  y = r * sin(angle)
- *  degree : 0-360
- *  n: 0 - ∞
- *  c: 0.5
- */
-
-/*TrailRenderer 组建实现拖尾
- * Lerping 控制拖尾运功轨迹与速度
- *
- *         Vector3.Lerp (from: Vector3, to: Vector3, t: float)
- * t 在 [0 - 1], t = 0时 返回from, t =1 时返回 to, t = 0.5 时返回 from 与 to 的平均值
- */
-public class Phyllotaxis : MonoBehaviour {
-
+public class PhyllotaxisTrail : MonoBehaviour {
     public AudioPeer _audioPeer;       //AudioPeer获得音乐文件的频谱
     private Material _trailMat;             //拖尾材质
     public Color _trailColor;                //拖尾颜色
@@ -31,6 +13,8 @@ public class Phyllotaxis : MonoBehaviour {
     private int _number;
     //拖尾渲染
     private TrailRenderer _trailRenderer;
+    //控制速度大小
+    public float TimerControl;
 
     //lerping 控制循环
     public bool _userlerping;  //用户控制是否使用Lerping
@@ -39,7 +23,7 @@ public class Phyllotaxis : MonoBehaviour {
     private bool _isLerping;    //判断lerp的开始与结束
     private Vector3 _startPosition, _endPosition;
     private int _currentIteration;   //当前Lerp的次数
-    private float _lerpPosTimer, _lerpPosSpeed;
+    public float _lerpPosTimer, _lerpPosSpeed;
     public Vector2 _lerpPosSpeedMinMax;
     public AnimationCurve _lerpPosAnimCurve;
     public int _lerpPosBand;
@@ -65,7 +49,8 @@ public class Phyllotaxis : MonoBehaviour {
     }
 
     // Use this for initialization
-    void Awake() {
+    void Awake()
+    {
         _trailRenderer = this.GetComponent<TrailRenderer>();
         _number = _numberStart;
         this.transform.localPosition = CalculatePhyllotaxis(_degree, _scale, _number);
@@ -89,58 +74,20 @@ public class Phyllotaxis : MonoBehaviour {
             if (_isLerping)
             {
                 _lerpPosSpeed = Mathf.Lerp(_lerpPosSpeedMinMax.x, _lerpPosSpeedMinMax.y, _lerpPosAnimCurve.Evaluate(_audioPeer._audioBand[_lerpPosBand]));
-                _lerpPosTimer = Time.deltaTime * _lerpPosSpeed;
+                _lerpPosTimer = Time.deltaTime * _lerpPosSpeed * TimerControl;
                 //Clamp01 将变换的位置设为时间的位置，钳制在0 - 1
                 this.transform.localPosition = Vector3.Lerp(_startPosition, _endPosition, Mathf.Clamp01(_lerpPosTimer));
                 if (_lerpPosTimer >= 1)
                 {
                     _lerpPosTimer -= 1;
                     _number += _stepSize;
+                    _currentIteration++;
                     SetLerpPosition();
                 }
 
             }
         }
-        if(!_userlerping)
-        {
-            //坐标随_number的增加而变化
-            _phyllotaxisPosition = CalculatePhyllotaxis(_degree, _scale, _number);
-            this.transform.localPosition = new Vector3(_phyllotaxisPosition.x, _phyllotaxisPosition.y, _lerpPosAnimCurve.Evaluate(_audioPeer._audioBand[_lerpPosBand]));
-
-
-            _number += _stepSize;
-            _currentIteration++;
-        }
-    }
-    /*private void FixedUpdate()
-    {
-        if (_userlerping)
-        {
-            if (_isLerping)
-            {
-                //计算轨迹运行时间
-                float timeSinceStarted = Time.time - _timeStartedLerping;
-                float percentageComplete = timeSinceStarted / _intervalLerp;
-                //在percentageComplete时间内从_startPosition运动到_endPosition 
-                this.transform.localPosition = Vector3.Lerp(_startPosition, _endPosition, percentageComplete);
-                //当时间大于1s时开始下一个循环
-                if (percentageComplete >= 0.97f)
-                {
-                    transform.localPosition = _endPosition;
-                    _number += _stepSize;
-                    _currentIteration++;
-                    if (_currentIteration <= _maxIteration)
-                    {
-                        StartLerping();
-                    }
-                    else
-                    {
-                        _isLerping = false;
-                    }
-                }
-            }
-        }
-        else
+        if (!_userlerping)
         {
             //坐标随_number的增加而变化
             _phyllotaxisPosition = CalculatePhyllotaxis(_degree, _scale, _number);
@@ -148,6 +95,6 @@ public class Phyllotaxis : MonoBehaviour {
             _number += _stepSize;
             _currentIteration++;
         }
-    }*/
+    }
 
 }
